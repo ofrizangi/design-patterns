75151f7eaaeac74356014af8daaa47c6600b1f45
ofrizangi <88827751+ofrizangi@users.noreply.github.com>
2022-02-13 22:09:32 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,73 @@
+// command pattern
+#include <iostream>
+
+using namespace std;
+
+class Light{
+public:
+    void lightOn(){
+        cout << "the light is on\n";
+    }
+    void lightOff(){
+        cout << "the light is off\n";
+    }
+};
+
+// command
+class Command{
+public:
+    virtual void execute() = 0;
+};
+
+//concrete command 1
+class turnOnCommand: public Command{
+    Light * light;
+public:
+    turnOnCommand(Light * light1){
+        this->light = light1;
+    }
+    void execute() override{
+        this->light->lightOn();
+    }
+};
+
+// concrete command 2
+class turnOffCommand: public Command{
+    Light * light;
+public:
+    turnOffCommand(Light * light1){
+        this->light = light1;
+    }
+    void execute() override{
+        this->light->lightOff();
+    }
+};
+
+// invoker
+class HandleLight{
+    Command * command;
+public:
+    HandleLight() = default;
+
+    void setCommand(Command * command){
+        this->command = command;
+    }
+
+    void BottomPressed(){
+        command->execute();
+    }
+};
+
+
+
+
+int main() {
+    Light * l1 = new Light;
+    Command * c1 = new turnOnCommand(l1);
+    Command * c2 = new turnOffCommand(l1);
+    HandleLight * h = new HandleLight();
+    h->setCommand(c1);
+    h->BottomPressed();
+    h->setCommand(c2);
+    h->BottomPressed();
+}
