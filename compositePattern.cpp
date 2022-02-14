18e583a8b78d83256f7f9f941be63ce242747d98
ofrizangi <88827751+ofrizangi@users.noreply.github.com>
2022-02-14 19:16:21 +0200

    Update compositePattern.cpp

--- compositePattern.cpp
+++ compositePattern.cpp
@@ -2,8 +2,13 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+
+class CycleException: public exception{
+};
 
 // we can't do employee abstract because of the vector (doesn't let putting abstract objects),
 // maybe try to implement in a different way by abstract
@@ -44,10 +49,36 @@ public:
     };
 };
 
-
+vector<Employee *> hasManager;
 //composite
 class Manager: public Employee{
     vector<Employee *> * v;
+
+private:
+
+    bool find(Employee * e){
+        for(vector<Employee *>:: iterator it = hasManager.begin(); it!= hasManager.end(); it++){
+            if(*it == e){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool ifHisMyManager(Employee * employee){
+        Manager * m = dynamic_cast<Manager *>(employee);
+        // if the dynamic casting didn't return null he is a manager
+        if(m!= nullptr){
+            if(this == employee)
+                return true;
+            for(vector<Employee *>:: iterator it = m->v->begin(); it!= m->v->end(); it++){
+                if( ifHisMyManager(*it)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 public:
     Manager(string name, int age) : Employee(name, age){
@@ -55,7 +86,11 @@ public:
     }
 
     void addEmployee(Employee * employee) override{
+        if(this == employee || ifHisMyManager(employee) || find(employee) ){
+            throw CycleException();
+        }
         this->v->push_back(employee);
+        hasManager.push_back(employee);
     }
 
     void removeEmployee(Employee * employee) override{
@@ -89,17 +124,49 @@ public:
 
 
 int main() {
-    Manager * m1 = new Manager("alice" , 35);
-    Engineer * e1 = new Engineer("bob" , 31);
-    Manager * m2 = new Manager("dave", 34);
-    Engineer * e2 = new Engineer("charlie" , 25);
+    Manager* m0=new Manager("alice" , 22);
+    Manager* m1=new Manager("alice" , 22);
+    Manager* m2=new Manager("alice" , 22);
+    Employee* e0=new Engineer("alice" , 22);
+    Employee* e1=new Engineer("alice" , 22);
+    Employee* e2=new Engineer("alice" , 22);
+    Employee* e3=new Engineer("alice" , 22);
+    m0->addEmployee(e0);
 
+    m0->addEmployee(m1);
     m1->addEmployee(e1);
+    m1->addEmployee(e2);
     m1->addEmployee(m2);
-    m2->addEmployee(e2);
-//    m1->removeEmployee(e1);
+    m2->addEmployee(e3);
+    try{
+        m1->addEmployee(e0);
+    }catch(CycleException e){
+        cout<<"catch 1"<<endl;
+    }
+    try{
+        m2->addEmployee(m0);
+    }catch(CycleException e){cout<<"catch 2"<<endl;}
+    try{
+        m1->addEmployee(m1);
+    }catch(CycleException e){ cout<<"catch 3"<<endl;}
+    delete m0;
 
-    m1->work();
+
+
+
+
+
+//    Manager * m1 = new Manager("alice" , 35);
+//    Engineer * e1 = new Engineer("bob" , 31);
+//    Manager * m2 = new Manager("dave", 34);
+//    Engineer * e2 = new Engineer("charlie" , 25);
+//
+//    m1->addEmployee(e1);
+//    m1->addEmployee(m2);
+//    m2->addEmployee(e2);
+////    m1->removeEmployee(e1);
+//
+//    m1->work();
 //    m2->work();
 
 
